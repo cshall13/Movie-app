c6518df9147a561e22f58af475101b04ad1cffc1
Shane Hall <Shane@Shanes-MacBook-Pro.local>
2017-05-25 15:46:41 -0400

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,16 @@ $(document).ready(function(){
 	const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key='+apiKey;
 	// console.log(nowPlayingUrl);
 
+	var buttonsHTML = ' ';
+	for(let i=0; i<genreArray.length; i++){
+		buttonsHTML +=`<button class="btn btn-default genre-button">${genreArray[i].name}</button>`;
+	}
+	$('#genre-buttons').html(buttonsHTML);
+
 	// Make AJAX request to the nowPlayingUrl
-	$.getJSON(nowPlayingUrl,(nowPlayingData)=>{
+	$.getJSON(nowPlayingUrl,function(nowPlayingData){
 		// console.log(nowPlayingData);
-		var  nowPlayingHTML = getHTML(nowPlayingData);
+		var nowPlayingHTML = getHTML(nowPlayingData);
 		$('#movie-grid').html(nowPlayingHTML);
 		$('.movie-poster').click(function(){
 			// change the html inside the modal
@@ -20,13 +26,26 @@ $(document).ready(function(){
 			$.getJSON(thisMovieUrl,(thisMovieData)=>{
 				console.log(thisMovieData);
 				$('#myModalLabel').html(thisMovieData.title);
-				$('.modal-body').html(thisMovieData.overview);
+				$('.modal-body').html(thisMovieData.overview);				
+				// $('.modal-content').html(thisMovieData.tagline);
 				// open the modal
 				$("#myModal").modal();
 			});
-		})
+		});
+		$grid = $('#movie-grid').isotope({
+			itemSelector: '.movie-poster'
+		});
+		$grid.imagesLoaded().progress( function() {
+  			$grid.isotope('layout');
+		});
+		$('.genre-button').click(function(){
+			console.log(this.innerText)
+			$grid.isotope({filter: '.'+this.innerText})
+		});
+		$('#all-genres').click(function(){
+			$grid.isotope({filter: ''})
+		});
 	});
-
 	$('#movie-form').submit((event)=>{
 		// dont submit form! JS will handle
 		event.preventDefault();
@@ -50,17 +69,31 @@ $(document).ready(function(){
 			// open the modal
 			$("#myModal").modal();
 				});
-			})
+			});
 		});
-
-
 	});
 
 	function getHTML(nowPlayingData){
 		var newHTML = '';
 			for(let i = 0; i < nowPlayingData.results.length; i++){
+				// set up 
+				 // var for the genre ids for THIS movie
+				var thisMovieGenres = nowPlayingData.results[i].genre_ids;
+				var movieGenreClassList = " ";
+				// loop through all known genres
+				for(let j=0; j<genreArray.length; j++){
+					// the genre that we are on (genreArray[j]), check to 
+					// see if it in THIS movies genre id list
+					if(thisMovieGenres.indexOf(genreArray[j].id)> -1){
+						// HIT! This genre_id is in THIS movie's genre_id list
+						// so we need to add the name to the class list
+						movieGenreClassList += genreArray[j].name + " ";
+					}
+					console.log(genreArray[j].name);
+				}
+				// console.log(movieGenreClassList);
 				var posterUrl = imageBaseUrl + 'w300' +nowPlayingData.results[i].poster_path;
-				newHTML += '<div class="col-sm-6 col-md-3 movie-poster" movie-id='+nowPlayingData.results[i].id+'>';
+				newHTML += '<div class="col-sm-6 col-md-3 movie-poster '+movieGenreClassList+'" movie-id='+nowPlayingData.results[i].id+'>';
 					newHTML += `<img src="${posterUrl}">`;
 				newHTML += `</div>`;
 		}
@@ -68,3 +101,15 @@ $(document).ready(function(){
 	}
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
